6ea857ee87604963fdbda3efc7ce4677f51350eb
Hanno Soo <hynt@ut.ee>
2018-11-18 20:04:13 +0000

    Sparks initial effort

---
+++ CG_VFX/Assets/Particles/Scripts/MoveSphere.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSphere : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        float moveUp = 0.0f;
+        if (Input.GetKeyDown("space"))
+        {
+            moveUp = 100.0f;
+        }
+
+        Vector3 movement = new Vector3(moveHorizontal, moveUp, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+    }
+}
---
+++ CG_VFX/Assets/Particles/Scripts/SparkController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SparkController : MonoBehaviour {
+
+    public ParticleSystem sparks;
+
+    // Use this for initialization
+    void Start () {
+        string name = this.name;
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(name + " collided with " + collision.collider.name);
+        sparks.Play();
+
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            sparks.transform.localPosition = transform.InverseTransformPoint(contact.point);
+            
+        }
+        //if (collision.relativeVelocity.magnitude > 2)
+            
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            sparks.transform.localPosition = transform.InverseTransformPoint(contact.point);
+            sparks.transform.LookAt(contact.point);
+        }
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log(name + " stopped with " + collision.collider.name);
+        sparks.Stop();
+    }
+}
