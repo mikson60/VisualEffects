44a8e5ef0604dc8319b8e752b21fbb365f9216f2
Hanno Soo <hynt@ut.ee>
2018-12-06 12:50:42 +0000

    Incremental sparks improvements

--- CG_VFX/Assets/Particles/Scripts/SparkController.cs
+++ CG_VFX/Assets/Particles/Scripts/SparkController.cs
@@ -27,15 +27,24 @@ public class SparkController : MonoBehaviour {
             ps = particleSystems[collision.collider];
         }
 
+        Debug.Log("Contacts " + collision.contacts.Length);
         foreach (ContactPoint contact in collision.contacts)
         {
             ps.transform.position = (contact.point);
             //ps.transform.LookAt(contact.normal);
         }
+
+        Debug.Log(collision.relativeVelocity.magnitude);
+
         ps.Play();
-        
-        //if (collision.relativeVelocity.magnitude > 2)
-            
+        var emRate = ps.emission.rateOverTime;
+        emRate.constantMin = sparks.emission.rateOverTime.constantMin * collision.relativeVelocity.magnitude;
+        emRate.constantMax = sparks.emission.rateOverTime.constantMax * collision.relativeVelocity.magnitude;
+        var v = ps.velocityOverLifetime.speedModifier;
+        v.constantMin = sparks.velocityOverLifetime.speedModifier.constantMin * collision.relativeVelocity.magnitude;
+        v.constantMax = sparks.velocityOverLifetime.speedModifier.constantMax * collision.relativeVelocity.magnitude;
+
+
     }
 
     private void OnCollisionStay(Collision collision)
@@ -43,11 +52,23 @@ public class SparkController : MonoBehaviour {
         if (particleSystems.ContainsKey(collision.collider))
         {
             ParticleSystem ps = particleSystems[collision.collider];
+            Debug.Log("Contacts " + collision.contacts.Length);
             foreach (ContactPoint contact in collision.contacts)
             {
                 ps.transform.position = (contact.point);
                 //ps.transform.LookAt(contact.normal);
             }
+
+            Debug.Log(collision.relativeVelocity.magnitude);
+            var emRate = ps.emission.rateOverTime;
+            emRate.constantMin = sparks.emission.rateOverTime.constantMin * collision.relativeVelocity.magnitude;
+            emRate.constantMax = sparks.emission.rateOverTime.constantMax * collision.relativeVelocity.magnitude;
+            var v = ps.velocityOverLifetime.speedModifier;
+            v.constantMin = sparks.velocityOverLifetime.speedModifier.constantMin * collision.relativeVelocity.magnitude;
+            v.constantMax = sparks.velocityOverLifetime.speedModifier.constantMax * collision.relativeVelocity.magnitude;
+
+            Debug.Log(v.constantMin + ", " + v.constantMax);
+            //Debug.Log(collision.contacts.Length);
         }
     }
 
